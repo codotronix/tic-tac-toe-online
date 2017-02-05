a554e68de5717be83f95b3d37ffb95a024d04f0b
Codotronix <codotronix@gmail.com>
2017-02-05 12:25:03 +0530

    Initial Commit

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+	$(window).on('resize', adjustSizes);
+
+	init();
+
+
+	function init () {
+		adjustSizes();
+	}
+
+	function adjustSizes () {
+		$('.cell').height($('.cell').width());
+		$('.sign').css('font-size', $('.cell-inner').height());
+	}
+
+	
+})
